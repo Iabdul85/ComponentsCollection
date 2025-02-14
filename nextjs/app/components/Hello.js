7d156dfd99cd843f0a92306968491bd09cd5c87d
Abdul Qayyum <95339531+Iabdul85@users.noreply.github.com>
2025-02-15 01:29:08 +0500

    eewr

---
+++ nextjs/app/components/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+         Hello World! Fuck You!!!
+      </div>
+    )
+  }
+}
+
+export default Hello
+
